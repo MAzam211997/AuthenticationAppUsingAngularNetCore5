abf9812e1dac9d3ba02f908dba3ec09747c4db83
MAzam211997 <mazam19971@gmail.com>
2021-07-17 09:19:59 +0500

    Added AuditableEntity

---
+++ AuthenticationApp/Models/AuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationApp.Models
+{
+    public class AuditableEntity
+    {
+        public string? CreatedBy { get; set; }
+        public string? ModifiedBy { get; set; }
+        public string? DeletedBy { get; set; }
+        //public string? ViewedBy { get; set; }
+        //public DateTime? ViewedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
